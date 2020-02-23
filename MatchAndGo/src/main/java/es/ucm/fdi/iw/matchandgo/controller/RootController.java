284510b14480fcc98e2c50300a28e20af2b05c27
EnriqueTorrijos <enritorr@ucm.es>
2020-02-23 20:47:52 +0100

    start mapping "/mensajes" in RootController

--- MatchAndGo/src/main/java/es/ucm/fdi/iw/matchandgo/controller/RootController.java
+++ MatchAndGo/src/main/java/es/ucm/fdi/iw/matchandgo/controller/RootController.java
@@ -9,10 +9,47 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import es.ucm.fdi.iw.matchandgo.controller.RootController.chatPersona.mensaje;
+
 @Controller
 public class RootController {
 
     private static Logger log = LogManager.getLogger(RootController.class);
 
-    //adsfadsf
+    //CLASE AUXILIAR PARA LA PANTALLA "mensajes"
+    private class chatPersona {
+        private String persona;
+
+        private class mensaje {
+            private String contenido;
+            private String sender; //La persona que lo envía
+
+            public mensaje(String c, String s) {
+                this.contenido = c;
+                this.sender = s;
+            }
+        }
+        private mensaje[] mensajes;
+
+        public chatPersona(String persona, mensaje[]mensajes) {
+            this.persona = persona;
+            this.mensajes = mensajes;
+        }
+    }
+
+
+    public chatPersona[] generarChatsPorDefecto() {
+        chatPersona[] res;
+        
+        //res[0] = chatPersona("Manuel", [{"Buenas tardes", "Pepe"}, ])
+
+        return res;
+    }
+
+    @GetMapping("/mensajes")
+    public Object mostrarMensajes(Model model, @RequestParam String persona) {
+
+        chatPersona[] chats = generarChatsPorDefecto();
+        return null;
+    }
 }
