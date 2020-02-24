f2b9610e906da6044aa255d64b6eff472d602f5e
Gasan <saninazer@gmail.com>
2020-02-24 21:23:32 +0100

    view moderador finished- added header and footer

--- MatchAndGo/src/main/java/es/ucm/fdi/iw/matchandgo/control/RootController.java
+++ MatchAndGo/src/main/java/es/ucm/fdi/iw/matchandgo/control/RootController.java
@@ -1,9 +1,10 @@
 package es.ucm.fdi.iw.matchandgo.control;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RootController {
@@ -13,8 +14,23 @@ public class RootController {
 			) { 
 			model.addAttribute("titulo", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."); // escribe en modelo
 			model.addAttribute("descripcion", "Orci a scelerisque purus semper eget duis at. Eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus.");
-			model.addAttribute("tags", "tag");
-			return "metchAndGoVistaModerador"; 
+			model.addAttribute("tags", new ArrayList<String>() {{
+				add("tag");
+				add("tag1");
+				add("tag2");
+				add("tag3");
+				add("tag4");
+				add("tag5");
+			}});
+			model.addAttribute("listnum", new ArrayList<Integer>() {{
+				add(1);
+				add(2);
+				add(3);
+				add(4);
+				add(5);
+				add(6);
+			}});
+			return "matchAndGoVistaModerador"; 
 	}
 }
 
