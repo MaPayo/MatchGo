19006146031590f3722c5a8e164a240564996282
Carlos <colano@ucm.es>
2020-04-05 12:00:35 +0200

    added websocket admin view, ajax petition to list all users, changed view admin (forms in post with ajax => :-( ) added some queryname user, change this model to add json

--- MatchAndGo/src/main/java/es/ucm/fdi/iw/control/AdminController.java
+++ MatchAndGo/src/main/java/es/ucm/fdi/iw/control/AdminController.java
@@ -1,7 +1,8 @@
 package es.ucm.fdi.iw.control;
 
+import java.util.List;
 import java.io.File;
-
+import javax.servlet.http.HttpSession;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import es.ucm.fdi.iw.LocalData;
 import es.ucm.fdi.iw.model.Usuario;
@@ -44,20 +46,32 @@ public class AdminController {
 		model.addAttribute("activeProfiles", env.getActiveProfiles());
 		model.addAttribute("basePath", env.getProperty("es.ucm.fdi.base-path"));
 
-		model.addAttribute("users", entityManager.createQuery(
-				"SELECT u FROM Usuario u").getResultList());
+	//	model.addAttribute("users", entityManager.createQuery(
+	//			"SELECT u FROM Usuario u").getResultList());
 		
-		model.addAttribute("allUsers", entityManager.createQuery(
-				"SELECT u FROM Usuario u").getResultList());
+	//	model.addAttribute("allUsers", entityManager.createQuery(
+	//			"SELECT u FROM Usuario u").getResultList());
 		model.addAttribute("allEvents", entityManager.createQuery(
 				"SELECT u FROM Evento u").getResultList());
 	
 		return "admin_view";
 	}
+
+	@GetMapping(path = "/userlist", produces = "application/json")
+	@Transactional
+	@ResponseBody
+	public List<Usuario.Transfer> retrieveUsers(HttpSession session){
+		log.info("Generating User List");
+		List<Usuario> usuarios = entityManager.createQuery(
+				"SELECT u FROM Usuario u").getResultList();
+		return Usuario.asTransferObjects(usuarios);
+	}
+
+
 	
 	@PostMapping("/toggleuser")
 	@Transactional
-	public String delUser(Model model,	@RequestParam long id) {
+	public String blockUser2(Model model,	@RequestParam long id) {
 		Usuario target = entityManager.find(Usuario.class, id);
 		if (target.isEnabled()) {
 			// disable
@@ -71,6 +85,32 @@ public class AdminController {
 			// enable user
 			target.setEnabled(true);
 		}
+		return index(model);
+	}	
+	@PostMapping("/blockUser")
+	@Transactional
+	public String blockUser(Model model, @RequestParam long id) {
+		Usuario target = entityManager.find(Usuario.class, id);
+		boolean newState = false;
+		if (target.isEnabled()){
+			newState = false;
+		} else {
+			newState = true;
+		}
+		entityManager.createNamedQuery("Usuario.blockUser")
+			.setParameter("idUser",id)
+			.setParameter("state",newState)
+			.executeUpdate();
+		return index(model);
+	}	
+
+
+	@GetMapping("/deleteUser")
+	@Transactional
+	public String deleteUser(Model model, @RequestParam long id) {		
+		entityManager.createNamedQuery("Usuario.deleteUser")
+			.setParameter("idUser",id)
+			.executeUpdate();
 		return index(model);
 	}	
 }
--- MatchAndGo/src/main/java/es/ucm/fdi/iw/model/Usuario.java
+++ MatchAndGo/src/main/java/es/ucm/fdi/iw/model/Usuario.java
@@ -1,7 +1,9 @@
 package es.ucm.fdi.iw.model;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Collection;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -15,10 +17,18 @@ import javax.persistence.OneToMany;
 
 
 
+/**
+ * @author colano
+ */
+
 @Entity
 @NamedQueries({
 	@NamedQuery(name="Usuario.byUsername", query= "SELECT u from Usuario u WHERE "
-			+ "u.nombre = :username")
+		+ "u.nombre = :username"),
+	@NamedQuery(name="Usuario.deleteUser", query= "DELETE FROM Usuario u WHERE "
+		+ "u.id = :idUser"),
+	@NamedQuery(name="Usuario.blockUser", query= "UPDATE Usuario u SET u.enabled = :state "
+		+ "WHERE u.id = :idUser")
 })
 
 public class Usuario {
@@ -26,7 +36,7 @@ public class Usuario {
 	public enum Role{
 		USER,ADMIN, MOD
 	}	
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
@@ -42,30 +52,193 @@ public class Usuario {
 	private String sexo;
 	private String roles;
 	private String imagen;
-	
+
 	private boolean enabled;
-	
+
 	@OneToMany(mappedBy="valorado")
 	private List<Valoracion> valoracionesRecibidas;
 
 	@OneToMany(mappedBy="valorante")
 	private List<Valoracion> valoracionesDadas;
-	
+
 	@OneToMany(mappedBy="sender")
 	private List<Mensaje> listaMensajesEnviados;
-	
+
 	@OneToMany(mappedBy="receiver")
 	private List<Mensaje> listaMensajesRecibidos;
 
 	@ManyToMany
 	private List<Tags> tags;
-	
+
 	@ManyToMany(mappedBy = "participantes")
 	private List<Evento> joinedEvents;
-	
+
 	@OneToMany(mappedBy="creador")
 	private List<Evento> createdEvents;
-	
+
+
+
+	/**
+	 * Convierte colecciones de mensajes a formato JSONificable
+	 * @param messages
+	 * @return
+	 * @throws JsonProcessingException
+	 */
+	public static List<Transfer> asTransferObjects(Collection<Usuario> users) {
+		ArrayList<Transfer> all = new ArrayList<>();
+		for (Usuario u : users) {
+			all.add(new Transfer(u));
+		}
+		return all;
+	}
+
+	/**
+	 * Objeto para persistir a/de JSON
+	 * @author mfreire
+	 * @author colano
+	 */
+
+	public static class Transfer {
+		public enum Role{
+			USER,ADMIN, MOD
+		}	
+		private long id;
+		private String nombre;
+		private String apellidos;
+		private String correo;
+		private String password;
+		private String fecha_nac;
+		private String sexo;
+		private String roles;
+		private String imagen;
+		private boolean enabled;
+		private List<Valoracion> valoracionesRecibidas;
+		private List<Valoracion> valoracionesDadas;
+		private List<Mensaje> listaMensajesEnviados;
+		private List<Mensaje> listaMensajesRecibidos;
+		private List<Tags> tags;
+		private List<Evento> joinedEvents;
+		private List<Evento> createdEvents;
+
+		public Transfer(Usuario m) {
+			this.id = m.getId();
+			this.nombre = m.getNombre();
+			this.apellidos = m.getApellidos();
+			this.roles = m.getRoles();
+			this.password = m.getPassword();
+			this.correo = m.getCorreo();
+			this.fecha_nac = m.getFecha_nac();
+			this.sexo = m.getSexo();
+		}
+		public long getId() {
+			return id;
+		}
+		public void setId(long id) {
+			this.id = id;
+		}
+		public String getNombre() {
+			return nombre;
+		}
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+		public String getApellidos() {
+			return apellidos;
+		}
+		public void setApellidos(String apellidos) {
+			this.apellidos = apellidos;
+		}
+		public String getCorreo() {
+			return correo;
+		}
+		public void setCorreo(String correo) {
+			this.correo = correo;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		public String getFecha_nac() {
+			return fecha_nac;
+		}
+		public void setFecha_nac(String fecha_nac) {
+			this.fecha_nac = fecha_nac;
+		}
+		public String getSexo() {
+			return sexo;
+		}
+		public void setSexo(String sexo) {
+			this.sexo = sexo;
+		}
+		public String getRoles() {
+			return roles;
+		}
+		public void setRoles(String roles) {
+			this.roles = roles;
+		}
+		public String getImagen() {
+			return imagen;
+		}
+		public void setImagen(String img) {
+			this.imagen = img;
+		}
+		public List<Valoracion> getValoracionesRecibidas() {
+			return valoracionesRecibidas;
+		}
+		public void setValoracionesRecibidas(List<Valoracion> valoracion) {
+			this.valoracionesRecibidas = valoracion;
+		}
+		public List<Valoracion> getValoracionesDadas() {
+			return valoracionesDadas;
+		}
+		public void setValoracionesDadas(List<Valoracion> valoracion) {
+			this.valoracionesDadas = valoracion;
+		}
+		public List<Tags> getTags() {
+			return tags;
+		}
+		public void setTags(List<Tags> tags) {
+			this.tags = tags;
+		}
+		public List<Mensaje> getListaMensajesEnviados() {
+			return listaMensajesEnviados;
+		}
+		public void setListaMensajesEnviados(List<Mensaje> listaMensajesEnviados) {
+			this.listaMensajesEnviados = listaMensajesEnviados;
+		}
+		public List<Mensaje> getListaMensajesRecibidos() {
+			return listaMensajesRecibidos;
+		}
+		public void setListaMensajesRecibidos(List<Mensaje> listaMensajesRecibidos) {
+			this.listaMensajesRecibidos = listaMensajesRecibidos;
+		}
+		public boolean isEnabled() {
+			return enabled;
+		}
+		public boolean hasRole(Role role) {
+			String roleName = role.name();
+			return Arrays.stream(roles.split(","))
+				.anyMatch(r -> r.equals(roleName));
+		}
+		public List<Evento> getJoinedEvents() {
+			return joinedEvents;
+		}
+		public void setJoinedEvents(List<Evento> joinedEvents) {
+			this.joinedEvents = joinedEvents;
+		}
+		public List<Evento> getCreatedEvents() {
+			return createdEvents;
+		}
+		public void setCreatedEvents(List<Evento> createdEvents) {
+			this.createdEvents = createdEvents;
+		}
+	}
+
+
+
+
 	@Override
 	public int hashCode() {
 		return Long.hashCode(getId());
@@ -75,7 +248,7 @@ public class Usuario {
 	public boolean equals(Object o) {
 		return ((Usuario)o).getId() == getId();
 	}
-	
+
 	/**
 	 * Checks whether this user has a given role.
 	 * @param role to check
@@ -84,10 +257,10 @@ public class Usuario {
 	public boolean hasRole(Role role) {
 		String roleName = role.name();
 		return Arrays.stream(roles.split(","))
-				.anyMatch(r -> r.equals(roleName));
+			.anyMatch(r -> r.equals(roleName));
 	}
-	
-	
+
+
 	public List<Evento> getJoinedEvents() {
 		return joinedEvents;
 	}
@@ -111,8 +284,8 @@ public class Usuario {
 	public Usuario() {
 		super();
 	}
-	
-	
+
+
 	public Usuario(long id, String nombre, String apellidos, String correo, String password, String fecha_nac, String sexo, String roles) {
 		this.id = id;
 		this.nombre = nombre;
@@ -127,92 +300,92 @@ public class Usuario {
 	public long getId() {
 		return id;
 	}
-	
+
 	public void setId(long id) {
 		this.id = id;
 	}
-	
+
 	public String getNombre() {
 		return nombre;
 	}
-	
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	
+
 	public String getApellidos() {
 		return apellidos;
 	}
-	
+
 	public void setApellidos(String apellidos) {
 		this.apellidos = apellidos;
 	}
-	
+
 	public String getCorreo() {
 		return correo;
 	}
-	
+
 	public void setCorreo(String correo) {
 		this.correo = correo;
 	}
-	
+
 	public String getPassword() {
 		return password;
 	}
-	
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	public String getFecha_nac() {
 		return fecha_nac;
 	}
-	
+
 	public void setFecha_nac(String fecha_nac) {
 		this.fecha_nac = fecha_nac;
 	}
-	
+
 	public String getSexo() {
 		return sexo;
 	}
-	
+
 	public void setSexo(String sexo) {
 		this.sexo = sexo;
 	}
-	
+
 	public String getRoles() {
 		return roles;
 	}
-	
+
 	public void setRoles(String roles) {
 		this.roles = roles;
 	}
-	
+
 	public String getImagen() {
 		return imagen;
 	}
-	
+
 	public void setImagen(String img) {
 		this.imagen = img;
 	}
-	
+
 
 	public List<Valoracion> getValoracionesRecibidas() {
 		return valoracionesRecibidas;
 	}
-	
+
 	public void setValoracionesRecibidas(List<Valoracion> valoracion) {
 		this.valoracionesRecibidas = valoracion;
 	}
-	
+
 	public List<Valoracion> getValoracionesDadas() {
 		return valoracionesDadas;
 	}
-	
+
 	public void setValoracionesDadas(List<Valoracion> valoracion) {
 		this.valoracionesDadas = valoracion;
 	}
-	
+
 
 	public List<Tags> getTags() {
 		return tags;
@@ -247,6 +420,6 @@ public class Usuario {
 	public void setEnabled(boolean enabled) {
 		this.enabled = enabled;
 	}
-	
+
 
 }
