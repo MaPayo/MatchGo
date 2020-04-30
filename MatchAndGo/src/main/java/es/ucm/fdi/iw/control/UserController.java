e0087e2f4960ec21f8907edb59fd8773bd1d3656
merchf <mercherr@ucm.es>
2020-04-30 22:27:42 +0200

    perfil super bonito y maravilloso con los tags, eventos y comentarios

--- MatchAndGo/src/main/java/es/ucm/fdi/iw/control/UserController.java
+++ MatchAndGo/src/main/java/es/ucm/fdi/iw/control/UserController.java
@@ -37,6 +37,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
@@ -301,6 +302,10 @@ public class UserController {
 		}
 		return false;
 	}
+	
+	
+	
+	
 	/**
 	 * Non-interactive authentication; user and password must already exist
 	 *
