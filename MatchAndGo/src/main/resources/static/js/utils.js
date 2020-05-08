335ca5650f724baefa16d10ae39395d4b952b5e4
carlos <colano@ucm.es>
2020-05-08 18:27:09 +0200

    a;ado los select de los formularios para comprobar los string que me pudieran pasar, en el lado del servidor => e500 si intenta transformar a tipo long

--- MatchAndGo/src/main/resources/static/js/utils.js
+++ MatchAndGo/src/main/resources/static/js/utils.js
@@ -1,7 +1,7 @@
 function validateStrings(form){
 	const badWords = "drop database|drop datatable|select.*from|delete.*from|update.*set|http|https|porno|viagra";
 	const re = new RegExp(badWords);
-	let inputs = form.querySelectorAll('input[type=text]');
+	let inputs = form.querySelectorAll('input[type=text],select');
 	for (var i = 0; i < inputs.length;i++){
 		if (re.test(inputs[i].value)){
 			alert("bad Boy");
