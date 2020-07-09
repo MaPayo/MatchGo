6913a7ee72f52533abe30071cf9fd443ac7aad4e
Carlos Olano <colano@ucm.es>
2020-07-09 20:14:55 +0200

    estilo

--- MatchAndGo/src/main/resources/static/js/profile.js
+++ MatchAndGo/src/main/resources/static/js/profile.js
@@ -2,17 +2,17 @@
 /**
  * Actions to perform once the page is fully loaded
  */
-  
-  document.addEventListener("DOMContentLoaded", () => {
-	  
-	  if (config.socketUrl) {
-			let subs = config.admin ? 
 
-				["/topic/admin", "/user/queue/updates"] : ["/user/queue/updates"]
-			ws.initialize(config.socketUrl, subs);
-		}
-	  
-	  /*
+document.addEventListener("DOMContentLoaded", () => {
+
+	if (config.socketUrl) {
+		let subs = config.admin ? 
+
+			["/topic/admin", "/user/queue/updates"] : ["/user/queue/updates"]
+		ws.initialize(config.socketUrl, subs);
+	}
+
+	/*
 	  document.getElementById("mostrarTags").addEventListener("click",function() {
 			document.getElementById("mostrarTags").classList.add("bgdarkgrey");
 			document.getElementById("listaEventosCreados").classList.remove("bgdarkgrey");
@@ -22,65 +22,73 @@
 			var lastURLSegment = pageURL.substr(pageURL.lastIndexOf('/') + 1);
 			go(config.rootUrl + "tag/listTags/"+ lastURLSegment,"POST",null).then(e => console.log("holaaaaaaaaaa"));
 
-	  
+
 	  });
-	  
-	  */
-	  //1 habilita modificar el perfil, 2 guarda los cambios
-	  document.getElementById("ModificarPerfil").addEventListener("click",function() {
-		  let boton =  document.getElementById("ModificarPerfil");
-		  if(boton.dataset.action == 1){
-			  boton.dataset.action = 2;
-			  let children = document.getElementById("datosBasicosPerfil").children;
-			  for (i = 0; i < children.length; i++) {
-				  children[i].removeAttribute("readonly");
-				  children[i].removeAttribute("disabled");
-			  }
-			  boton.value = "Guardar cambios";
-		  }else{
-			 boton.dataset.action = 1;
-			  let name = document.getElementById("nombre").value;
-			 	let fecha = document.getElementById("edad").value;
-			 	let gender = document.getElementById("genderUser").value;
-			  go(config.rootUrl + "user/modificarPerfil/","POST",{name:name, fecha:fecha, gender:gender});
-			  
-				document.getElementById("nombre").readOnly= true;
-			 	document.getElementById("edad").readOnly= true;
-			 	document.getElementById("genderUser").setAttribute("disabled",true);
-			 	boton.value = "Modificar perfil";
-		  }
-	  });  
-		  
-		  
-	  
-	  
-	 
-	  
-	  
-	  document.getElementById("listaTags").addEventListener("click",function() {
-		  if(event.target.matches("img")){
-				const id = event.target.dataset.tag;
-				go(config.rootUrl + "tag/deleteTagUser/"+id,"POST",null);
 
-		  }
-	  });
-	  document.getElementById("botonAnadirTag").addEventListener("click",function() {
-			
-		  	let idTag = document.getElementById("tagSeleccionada").value;
-
-			go(config.rootUrl + "tag/addTagUser/"+idTag,"POST",null);
-
-	  
-	  });
-	  
-	  document.getElementById("tagInput").addEventListener("keypress",function(key) {
-			if (key.keyCode == 13){ //pulsa enter
-				let tag = document.getElementById("tagInput").value;
-				go(config.rootUrl + "tag/newTag","POST",{tagName:tag}).then();
-
+*/
+	//1 habilita modificar el perfil, 2 guarda los cambios
+	document.getElementById("ModificarPerfil").addEventListener("click",function() {
+		let boton =  document.getElementById("ModificarPerfil");
+		if(boton.dataset.action == 1){
+			boton.dataset.action = 2;
+			let children = document.getElementById("datosBasicosPerfil").children;
+			for (i = 0; i < children.length; i++) {
+				children[i].removeAttribute("readonly");
+				children[i].removeAttribute("disabled");
 			}
+			boton.value = "Guardar cambios";
+		}else{
+			boton.dataset.action = 1;
+			let name = document.getElementById("nombre").value;
+			let fecha = document.getElementById("edad").value;
+			let gender = document.getElementById("genderUser").value;
+			go(config.rootUrl + "user/modificarPerfil/","POST",{name:name, fecha:fecha, gender:gender});
+
+			document.getElementById("nombre").readOnly= true;
+			document.getElementById("edad").readOnly= true;
+			document.getElementById("genderUser").setAttribute("disabled",true);
+			boton.value = "Modificar perfil";
+		}
+	});  
+
+
+	const pitchers = document.getElementsByName("por");
+	for (j=0;j<pitchers.length;j++){
+		pitchers[j].addEventListener("change",function(){
+			const targets = document.getElementByClassName("event-item");
+			for (i = 0; i < targets.length;i++){
+				targets[i].removeAttribute("hidden");
+			}
+			alert(this.value);
 		});
-	  
+	}
+
+
+
+	document.getElementById("listaTags").addEventListener("click",function() {
+		if(event.target.matches("img")){
+			const id = event.target.dataset.tag;
+			go(config.rootUrl + "tag/deleteTagUser/"+id,"POST",null);
+
+		}
+	});
+	document.getElementById("botonAnadirTag").addEventListener("click",function() {
+
+		let idTag = document.getElementById("tagSeleccionada").value;
+
+		go(config.rootUrl + "tag/addTagUser/"+idTag,"POST",null);
+
+
+	});
+
+	document.getElementById("tagInput").addEventListener("keypress",function(key) {
+		if (key.keyCode == 13){ //pulsa enter
+			let tag = document.getElementById("tagInput").value;
+			go(config.rootUrl + "tag/newTag","POST",{tagName:tag}).then();
+
+		}
+	});
+
 
 	//response.forEach(e => );
 
@@ -108,7 +116,7 @@ function listUsers(jsonArray, type){
 			while (select.firstChild ) {
 				select.removeChild(select.lastChild);
 			}
-			
+
 			jsonArray.forEach(e => appendChild(e,type));
 			break;
 		case "selectTag":
@@ -144,12 +152,12 @@ function appendChild(element, type){
 			html =["<option value="+element.id+">"+element.tag+"</option>"];
 			break;
 		case "updateUserPage":
-		/*	modificarId="datosBasicosPerfil";
+			/*	modificarId="datosBasicosPerfil";
 			html=["<h2 id='nombre'> <span" + element.firstName + "></span></h2>" ];
-             th:text="${user.firstName} "></span></h2>      
-            <p id="edad"><span th:text="${user.birthDate}"></span></p>	   
-            <p id="sexo"><span th:text="${user.gender}"></span></p>	 
-*/
+	     th:text="${user.firstName} "></span></h2>      
+	    <p id="edad"><span th:text="${user.birthDate}"></span></p>	   
+	    <p id="sexo"><span th:text="${user.gender}"></span></p>	 
+	    */
 			break
 	}
 	document.getElementById(modificarId).insertAdjacentHTML('beforeend',html);
